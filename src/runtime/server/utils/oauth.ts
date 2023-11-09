1a07be417ffe63ba4c787842dde8965c9aaa4d68
FTurrin <fturrin@alteanet.it>
2023-11-09 12:43:20 +0100

    add azure to oauth.ts

--- src/runtime/server/utils/oauth.ts
+++ src/runtime/server/utils/oauth.ts
@@ -2,10 +2,13 @@ import { githubEventHandler } from '../lib/oauth/github'
 import { googleEventHandler } from '../lib/oauth/google'
 import { spotifyEventHandler } from '../lib/oauth/spotify'
 import { twitchEventHandler } from '../lib/oauth/twitch'
+import { azureEventHandler } from '../lib/oauth/azure'
+
 
 export const oauth = {
   githubEventHandler,
   spotifyEventHandler,
   googleEventHandler,
   twitchEventHandler,
+  azureEventHandler,
 }
