033e5606efd268e7607b834b0248dbbb5e0f44f7
Piergiuseppe Longo <piergiuseppe.longo@gmail.com>
2023-11-09 17:17:09 +0100

    Fix azure

--- src/runtime/server/lib/oauth/azure.ts
+++ src/runtime/server/lib/oauth/azure.ts
@@ -5,18 +5,18 @@ import { ofetch } from 'ofetch'
 import { defu } from 'defu'
 import { useRuntimeConfig } from '#imports'
 
-export interface OAuthAzureConfig  {
+export interface OAuthAzureConfig {
   /**
    * Azure Client ID
    * @default process.env.NUXT_AZURE_AD_CLIENT_ID
    */
   clientId?: string
 
-   /**
-   * Azure OAuth Client Secret
-   * @default process.env.NUXT_OAUTH_AZURE_AD_CLIENT_SECRET
-   */
-   clientSecret?: string
+  /**
+  * Azure OAuth Client Secret
+  * @default process.env.NUXT_OAUTH_AZURE_AD_CLIENT_SECRET
+  */
+  clientSecret?: string
 
   /**
    * Azure OAuth Scope
@@ -38,6 +38,9 @@ export interface OAuthAzureConfig  {
    * Azure OAuth Token URL
    */
   tokenURL?: string
+
+  tenant?: string
+
 }
 
 interface OAuthConfig {
@@ -46,14 +49,24 @@ interface OAuthConfig {
   onError?: (event: H3Event, error: H3Error) => Promise<void> | void
 }
 
-export function azureEventHandler({ config, onSuccess, onError }: OAuthConfig) {
+export function azureEventHandler ({ config, onSuccess, onError }: OAuthConfig) {
+
   return eventHandler(async (event: H3Event) => {
+
     // @ts-ignore
     config = defu(config, useRuntimeConfig(event).oauth?.azure, {
-      authorizationURL: 'https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize',
-      tokenURL: 'https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token'
+
     }) as OAuthAzureConfig
     const { code } = getQuery(event)
+
+    if (!config.tenant) {
+      const error = createError({
+        statusCode: 500,
+        message: 'Missing NUXT_OAUTH_AZURE_AD_TENANT_ID env variables.'
+      })
+      if (!onError) throw error
+      return onError(event, error)
+    }
 
     if (!config.clientId) {
       const error = createError({
@@ -65,15 +78,17 @@ export function azureEventHandler({ config, onSuccess, onError }: OAuthConfig) {
     }
 
     const redirectUrl = getRequestURL(event).href
+    const authorizationURL = `https://login.microsoftonline.com/${config.tenantId}/oauth2/v2.0/authorize`,
+    const tokenURL = `https://login.microsoftonline.com/${config.tenantId}/oauth2/v2.0/token`
     if (!code) {
       config.scope = config.scope || []
       if (config.emailRequired && !config.scope.includes('user.read')) {
         config.scope.push('user.read')
       }
-      // Redirect to Azure  Oauth page
+      // Redirect to Azure Oauth page
       return sendRedirect(
         event,
-        withQuery(config.authorizationURL as string, {
+        withQuery(authorizationURL as string, {
           response_type: 'code',
           client_id: config.clientId,
           redirect_uri: redirectUrl,
@@ -83,48 +98,50 @@ export function azureEventHandler({ config, onSuccess, onError }: OAuthConfig) {
     }
 
     const tokens: any = await ofetch(
-      config.tokenURL as string,
+      tokenURL as string,
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        params: {
+        body: new URLSearchParams({
           grant_type: 'authorization_code',
           redirect_uri: parsePath(redirectUrl).pathname,
           client_id: config.clientId,
           client_secret: config.clientSecret,
           code
-        }
+        })
       }
     ).catch(error => {
       return { error }
     })
+
     if (tokens.error) {
       const error = createError({
         statusCode: 401,
-        message: `Azure login failed: ${tokens.error?.data?.error_description || 'Unknown error'}`,
+        message: `Azure login failed: ${tokens.error}`,
         data: tokens
       })
       if (!onError) throw error
       return onError(event, error)
     }
 
-    async function fetchAzureUserInfo(accessToken: string): Promise<any> {
-      const graphApiEndpoint = 'https://graph.microsoft.com/v1.0/me';
-    
+    async function fetchAzureUserInfo (accessToken: string): Promise<any> {
+      const graphApiEndpoint = 'https://graph.microsoft.com/v1.0/me'
+
       const response = await ofetch(graphApiEndpoint, {
         headers: {
           Authorization: `Bearer ${accessToken}`
         }
-      });
-    
-      const userInfo = await response.json();
-      return userInfo;
-    }
-    const users: any = await fetchAzureUserInfo(tokens.access_token);
+      })
 
-    const user = users;
+
+      return await response
+    }
+
+    const users: any = await fetchAzureUserInfo(tokens.access_token)
+
+    const user = users
 
     if (!user) {
       const error = createError({
